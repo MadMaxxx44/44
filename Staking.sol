ed4caa1adfe63d9201d3966431e3e0490f8cea6a
Maximum44 <85509687+MadMaxxx44@users.noreply.github.com>
2022-08-31 18:33:22 +0300

    Update and rename StakingV2.sol to Staking.sol

--- Staking.sol
+++ Staking.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import "./IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";                         
-import "@openzeppelin/contracts/access/Ownable.sol";                                              
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./SafeERC20.sol";                        
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract StakingV2 is Ownable, ReentrancyGuard{
-    
-    IERC20 public RewardToken;             
-    uint public TotalUsers;  
-    uint public Time = 60;
+contract Staking is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+    address public rewardToken;             
+    uint public totalUsers;  
+    uint public time = 60;
 
     event NewStaker(uint _amount, uint _stakingTime);
-    event TokenChanged(IERC20 _token);
+    event TokenChanged(address _token);
     event RewardClaimed(uint result);
     event NewPoolCreated(address _token, uint _percentReward, uint _priceToStart);
     
@@ -32,16 +32,16 @@ contract StakingV2 is Ownable, ReentrancyGuard{
     mapping(address => mapping(address => UserInfo)) public users;
     mapping(address => PoolInfo) public poolInfo;
 
-    constructor(IERC20 _token) {
-        RewardToken = _token;
+    constructor(address _token) {
+        rewardToken = _token;
     }
 
     function selfDestruct(address payable _owner) public onlyOwner{
         selfdestruct(_owner);
     }
 
-    function changeToken(IERC20 _token) public onlyOwner{    
-        RewardToken = _token;
+    function changeToken(address _token) public onlyOwner{    
+        rewardToken = _token;
         emit TokenChanged(_token);
     } 
     
@@ -53,7 +53,7 @@ contract StakingV2 is Ownable, ReentrancyGuard{
 
     // 1 period = 60 sec, for easily testing
     function calculateReward(uint _amount, uint _stakingTime, uint _percent) public view returns(uint reward){
-        uint periodPassed = _stakingTime/Time;
+        uint periodPassed = _stakingTime/time;
         return _amount*(_percent*periodPassed)/100;
     }
 
@@ -68,14 +68,14 @@ contract StakingV2 is Ownable, ReentrancyGuard{
     function stake(uint _amount, address _token, uint _stakingTime) public nonReentrant {
         PoolInfo storage pool = poolInfo[_token];
         UserInfo storage user = users[_token][msg.sender];
-        require(_stakingTime >= Time, "Wrong staking period");
+        require(_stakingTime >= time, "Wrong staking period");
         require(_amount >= pool.priceToStart, "Not enough amount to start");
-        IERC20(pool.token).transferFrom(msg.sender, address(this), _amount);
+        IERC20(pool.token).safeTransferFrom(msg.sender, address(this), _amount);
         user.amount = _amount;
         user.timeStart = block.timestamp;
         user.stakingTime = _stakingTime;
         pool.poolBalance = pool.poolBalance + _amount;
-        TotalUsers++;
+        totalUsers++;
         emit NewStaker(_amount, _stakingTime);
     }
 
@@ -84,13 +84,13 @@ contract StakingV2 is Ownable, ReentrancyGuard{
         UserInfo storage user = users[_token][msg.sender];
         require(block.timestamp - users[_token][msg.sender].timeStart >= users[_token][msg.sender].stakingTime, "Not enough time passed");
         uint result = calculateReward(user.amount, user.stakingTime, pool.percentReward);
-        IERC20(pool.token).transfer(msg.sender, user.amount);
-        IERC20(RewardToken).mint(msg.sender, result);
+        IERC20(pool.token).safeTransfer(msg.sender, user.amount);
+        IERC20(rewardToken).mint(msg.sender, result);
         pool.poolBalance = pool.poolBalance - user.amount;
         user.amount = 0;
         user.timeStart = 0;
         user.stakingTime = 0;
-        TotalUsers--;
+        totalUsers--;
         emit RewardClaimed(result);
         return result;                            
     }
