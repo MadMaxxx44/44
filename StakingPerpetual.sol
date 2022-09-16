b10ac475b0025659f3b4feb0c13e83ef04ffbbb6
Maximum44 <85509687+MadMaxxx44@users.noreply.github.com>
2022-09-16 20:53:29 +0300

    Update StakingPerpetual.sol

--- StakingPerpetual.sol
+++ StakingPerpetual.sol
@@ -2,20 +2,20 @@
 
 pragma solidity ^0.8.16;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./SafeERC20.sol";                        
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";            
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract StakingPerpetual is Ownable, ReentrancyGuard {
-    using SafeERC20 for IERC20;
-    IERC20 public rewardToken; //contract got to own this token, to mint reward for stakers           
+    using SafeERC20 for IERC20;          
     uint public totalUsers;
-    uint public Period;
+    uint public Period; //time in seconds after which user accumulate percentReward
 
     event NewStaker(uint _amount);
     event TokenChanged(IERC20 _token);
     event RewardClaimed(uint result);
     event NewPoolCreated(IERC20 _token, uint _percentReward, uint _priceToStart);
+    event TokenGranted(IERC20 _token, uint _amount); 
     
     struct PoolInfo {
         IERC20 token;
@@ -23,9 +23,9 @@ contract StakingPerpetual is Ownable, ReentrancyGuard {
         uint priceToStart;
         uint poolBalance;
         uint poolLimit;
+        uint poolGrant; //pool got to be granted before users can unstake their assets 
         uint minimum; //minimum time in seconds that user can stake tokens
-        address tokenOwner; //when create a pool, token owner got to transfer ownership to contract
-    }                       //this variable created for transfering ownership back to owner after
+    }                       
 
     struct UserInfo {
         uint amount;
@@ -35,47 +35,50 @@ contract StakingPerpetual is Ownable, ReentrancyGuard {
     mapping(IERC20 => mapping(address => UserInfo)) public users;
     mapping(IERC20 => PoolInfo) public poolInfo;
 
-    constructor(IERC20 _token) {
-        rewardToken = _token;
+    constructor() {
         Period = 60; // for conveniet testing
     }
 
+    fallback() external {}
+    receive() external payable {}
+
     function selfDestruct(address payable _owner) public onlyOwner {
         selfdestruct(_owner);
-    }
-    //when change token dont forget to transfer ownership
-    function changeToken(IERC20 _token) public onlyOwner {    
-        rewardToken = _token;
-        emit TokenChanged(_token);
-    }  
+    } 
     //owner set period in seconds, this will impact on calculateReward function
     //thus owner can regulate stakers rewards for example when Period = 60 and pool percentReward = 10, 
-    //staker will receive 10 tokens as reward from 100 tokens staked within 1 min
+    //staker will receive 10 tokens as reward from 100 tokens staked every minute
     function setPeriod(uint _period) public onlyOwner {              
         Period = _period; 
     }
-        
+    //calculate rewards for stakers
     function calculateReward(IERC20 _token) public view returns(uint) {
         PoolInfo storage pool = poolInfo[_token];
         UserInfo storage user = users[_token][msg.sender];
         require(user.amount > 0, "You do not have staked assets");
         return user.amount*pool.percentReward/100 * timePassed(_token) / Period;
     }
-
+    //calculate passed time for staker 
     function timePassed(IERC20 _token) public view returns(uint) {
         UserInfo storage user = users[_token][msg.sender];
         require(user.amount > 0, "You do not have staked assets");
         return block.timestamp - user.timeStart; 
     }
+    
+    function grantToken(IERC20 _token, uint _amount) public nonReentrant {
+        PoolInfo storage pool = poolInfo[_token];
+        IERC20(pool.token).safeTransferFrom(msg.sender, address(this), _amount); 
+        pool.poolGrant = pool.poolGrant + _amount; 
+        emit TokenGranted(_token, _amount); 
+    }
 
-    function poolInstance(IERC20 _token, uint _percentReward, uint _priceToStart, uint _poolLimit, uint _minimum, address _tokenOwner) public onlyOwner {
+    function poolInstance(IERC20 _token, uint _percentReward, uint _priceToStart, uint _poolLimit, uint _minimum) public onlyOwner {
         PoolInfo storage pool = poolInfo[_token];
         pool.token = _token; 
         pool.percentReward = _percentReward;
         pool.priceToStart = _priceToStart;
         pool.poolLimit = _poolLimit;
         pool.minimum = _minimum;
-        pool.tokenOwner = _tokenOwner; 
         emit NewPoolCreated(_token, _percentReward, _priceToStart);
     }
 
@@ -84,7 +87,7 @@ contract StakingPerpetual is Ownable, ReentrancyGuard {
         UserInfo storage user = users[_token][msg.sender];
         require(Period >= 60, "incorrect period");
         require(pool.poolBalance != pool.poolLimit, "sold out"); 
-        require(pool.poolBalance + _amount <= pool.poolLimit, "reduce amount"); 
+        require(pool.poolBalance + _amount <= pool.poolLimit, "reduce amount"); //prevents overflow of pool limit
         require(_amount >= pool.priceToStart, "Not enough amount to start");
         require(user.amount == 0, "you can not stake again"); 
         IERC20(pool.token).safeTransferFrom(msg.sender, address(this), _amount);
@@ -98,11 +101,13 @@ contract StakingPerpetual is Ownable, ReentrancyGuard {
     function unstake(IERC20 _token) public nonReentrant {
         PoolInfo storage pool = poolInfo[_token];
         UserInfo storage user = users[_token][msg.sender];
-        require(user.amount > 0, "u need to stake first");
+        uint reward = calculateReward(_token);
+        require(user.amount > 0, "you need to stake first");
+        require(pool.poolGrant >= reward, "token not granted");
         require(timePassed(_token) >= pool.minimum, "not enough minimum staking time passed");
-        uint reward = calculateReward(_token);  
-        IERC20(rewardToken).mint(msg.sender, user.amount + reward); 
+        IERC20(_token).safeTransfer(msg.sender, user.amount + reward); 
         pool.poolBalance = pool.poolBalance - user.amount;
+        pool.poolGrant = pool.poolGrant - reward;
         user.amount = 0;
         user.timeStart = 0;
         totalUsers--;
