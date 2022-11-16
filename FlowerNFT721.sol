ba1ce6ce72158b45d32a25c1a271f54f6eb9c848
Maximum44 <85509687+MadMaxxx44@users.noreply.github.com>
2022-11-16 12:20:54 +0200

    Update FlowerNFT721.sol

--- FlowerNFT721.sol
+++ FlowerNFT721.sol
@@ -2,7 +2,7 @@
 
 //deployed at rinkeby 0x08915522a235d5c906621a4ae60a7c360e8a599b
 
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
