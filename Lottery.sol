03ca81e6691cec3669443dac3bffe1766c89d213
Maximum44 <85509687+MadMaxxx44@users.noreply.github.com>
2023-01-25 10:56:37 +0200

    Create Lottery.sol

---
+++ Lottery.sol
@@ -0,0 +1,112 @@
+pragma solidity ^0.8.17;
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract Lottery is VRFConsumerBaseV2, Ownable { 
+    uint16 requestConfirmations = 3;
+    uint32 callbackGasLimit = 300000;
+    uint32 numWords = 2;
+    uint64 s_subscriptionId; 
+    address payable[] public players;
+    address payable public recentWinner;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    VRFCoordinatorV2Interface COORDINATOR;
+    bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
+    uint256 public fee;
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+
+    struct RequestStatus {
+        bool fulfilled;
+        bool exists;
+        uint256[] randomWords; 
+    }
+
+    mapping(uint => RequestStatus) public s_requests;
+
+    constructor(
+        uint64 _subscriptionId,
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        uint256 _fee
+    ) public VRFConsumerBaseV2(_vrfCoordinator) {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator); 
+        fee = _fee;
+        s_subscriptionId = _subscriptionId;
+    }
+
+    function enter() public payable {
+        // $50 minimum
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
+        players.push(payable(msg.sender));
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10; // 18 decimals
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
+
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Can't start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public onlyOwner returns(uint requestId){
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        s_requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        return(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint _requestId, 
+        uint256[] memory _randomWords)
+        internal override {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't there yet!"
+        );
+        require(s_requests[_requestId].exists, "request not found");
+        uint256 indexOfWinner = _randomWords[0] % players.length;
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+        // Reset
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        emit RequestFulfilled(_requestId, _randomWords);
+    }
+}
